336b3976eda3e4a0f628e934e972fab992d18da7
jbacon <julien.bacon.1@gmail.com>
2020-10-24 10:48:13 -0400

    adding app skeleton

---
+++ src/appCmd/appCmd.c
@@ -0,0 +1,82 @@
+/**
+ * Copyright (C) 2020 by Electronya
+ *
+ * @file      appCmd.c
+ * @author    jbacon
+ * @date      2020-10-07
+ * @brief     app Command Inplementation
+ *
+ * This file is the emplementation of the app command.
+ *
+ * @addtogroup  appCmd
+ */
+
+#include <appCmd.h>
+
+/**
+ * Execute the app name command
+ *
+ * @param shell     Handle to the shell
+ * @param argc      Command argument count
+ * @param argv      Pointer to the array of arguments
+ *
+ * @return 0 if successful, -1 otherwise
+ */
+static int execName(const struct shell *shell, size_t argc, char **argv)
+{
+  ARG_UNUSED(argc);
+  ARG_UNUSED(argv);
+
+  shell_print(shell, APP_INFO_TITLE);
+  shell_print(shell, APP_NAME);
+
+  return 0;
+}
+
+/**
+ * Execute the app version command
+ *
+ * @param shell     Handle to the shell
+ * @param argc      Command argument count
+ * @param argv      Pointer to the array of arguments
+ *
+ * @return 0 if successful, -1 otherwise
+ */
+static int execVersion(const struct shell *shell, size_t argc, char **argv)
+{
+  ARG_UNUSED(argc);
+  ARG_UNUSED(argv);
+
+  shell_print(shell, APP_INFO_TITLE);
+  shell_print(shell, APP_VERSION);
+
+  return 0;
+}
+
+/**
+ * Execute the app info command
+ *
+ * @param shell     Handle to the shell
+ * @param argc      Command argument count
+ * @param argv      Pointer to the array of arguments
+ *
+ * @return 0 if successful, -1 otherwise
+ */
+static int execInfo(const struct shell *shell, size_t argc, char **argv)
+{
+  ARG_UNUSED(argc);
+  ARG_UNUSED(argv);
+
+  shell_print(shell, APP_INFO_TITLE);
+  shell_print(shell, APP_NAME);
+  shell_print(shell, APP_VERSION);
+
+  return 0;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(app_sub,
+	SHELL_CMD(name, NULL, APP_NAME_USAGE, execName),
+	SHELL_CMD(version, NULL, APP_VER_UAGE, execVersion),
+	SHELL_CMD(info, NULL, APP_INFO_USAGE, execInfo),
+	SHELL_SUBCMD_SET_END);
+SHELL_CMD_REGISTER(app, &app_sub, APP_CMD_USAGE,	NULL);
---
+++ src/main.c
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2020 by Electronya
+ *
+ * @file      main.c
+ * @author    jbacon
+ * @date      2020-10-07
+ * @brief     TV Bench Controller LED Coprocessor Firmware
+ *
+ * This file is the entry point for the Electronya TV Bench Controller LED driving coprocessor.
+ */
+
+#include <zephyr.h>
+// #include <logging/log.h>
+#include <sys/printk.h>
+
+void main(void)
+{
+  printk("Hello World!");
+}
