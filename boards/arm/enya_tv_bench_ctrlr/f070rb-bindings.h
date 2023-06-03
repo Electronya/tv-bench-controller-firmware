509f47810c3c1c0dd6a702bac0777ee26c730154
jbacon <julien.baccon.1@gmail.com>
2023-06-03 07:20:59 -0400

    finalizing device tree binding for sk6812

--- boards/arm/enya_tv_bench_ctrlr/f070rb-bindings.h
+++ boards/arm/enya_tv_bench_ctrlr/f070rb-bindings.h
@@ -23,4 +23,22 @@
 #define B1414_ZERO_FRAME	0x60
 #define B1414_ONE_FRAME		0x7C
 
+/*
+ * SK6812 LED controller;
+ *
+ * Each bit of the control signal (waveform) is described with a 1.2 us pulse:
+ * 0 bit: 300 ns high and 900 ns low.
+ * 1 bit: 600 ns high and 600 ns low.
+ *
+ * At 6 MHz, one bit represents 166.666 ns.
+ * 1200 ns ->  7.2  bits
+ *  300 ns ->  1.8  bits
+ *  600 ns ->  3.6  bits
+ *  900 ns ->  5.4  bits
+ */
+#define SK6812_SPI_FREQ		6000000
+#define SK6812_ZERO_FRAME	0x60
+#define SK6812_ONE_FRAME	0x70
+#define SK6812_RST_DELAY  80
+
 #endif
