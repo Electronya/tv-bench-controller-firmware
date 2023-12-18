2029259dedcfafdc5788f9100f428f42876684cb
jbacon <julien.bacon.1@gmail.com>
2023-12-18 07:04:32 -0500

    adding static function to calculate color range

--- src/colorManager/colorManager.c
+++ src/colorManager/colorManager.c
@@ -24,6 +24,35 @@
 /* Setting module logging */
 LOG_MODULE_REGISTER(COLOR_MNGR_MODULE_NAME);
 
+/**
+ * @brief   Calcultate the new color based on the color wheel position.
+ *
+ * @param wheelPos  The color wheel position.
+ * @param red       The calculated red value.
+ * @param green     The calculated green value.
+ * @param blue      The calculated blue value.
+ */
+static void calculateNewColor(uint8_t wheelPos, uint8_t *red,
+                              uint8_t *green, uint8_t *blue)
+{
+  if(wheelPos < COLOR_WHEEL_BLU_TO_GRN)
+  {
+    *red = 255 - wheelPos * 3;
+    *blue = wheelPos * 3;
+  }
+  else if(wheelPos >= COLOR_WHEEL_BLU_TO_GRN &&
+    wheelPos < COLOR_WHEEL_GRN_TO_RED)
+  {
+    *blue = 255 - (wheelPos - COLOR_WHEEL_BLU_TO_GRN) * 3;
+    *green = (wheelPos - COLOR_WHEEL_BLU_TO_GRN) * 3;
+  }
+  else
+  {
+    *green = 255 - (wheelPos - COLOR_WHEEL_GRN_TO_RED) * 3;
+    *red = (wheelPos - COLOR_WHEEL_GRN_TO_RED) * 3;
+  }
+}
+
 void colorMngrSetSingle(Color_t *color, ZephyrRgbPixel_t *pixels,
                         size_t pixelCnt)
 {
@@ -92,22 +121,7 @@ void colorMngrUpdateRange(uint8_t wheelStart, uint8_t wheelEnd, bool reset,
   if(reset)
     wheelPos = wheelStart;
 
-  if(wheelPos < COLOR_WHEEL_BLU_TO_GRN)
-  {
-    red = 255 - wheelPos * 3;
-    blue = wheelPos * 3;
-  }
-  else if(wheelPos >= COLOR_WHEEL_BLU_TO_GRN &&
-    wheelPos < COLOR_WHEEL_GRN_TO_RED)
-  {
-    blue = 255 - (wheelPos - COLOR_WHEEL_BLU_TO_GRN) * 3;
-    green = (wheelPos - COLOR_WHEEL_BLU_TO_GRN) * 3;
-  }
-  else
-  {
-    green = 255 - (wheelPos - COLOR_WHEEL_GRN_TO_RED) * 3;
-    red = (wheelPos - COLOR_WHEEL_GRN_TO_RED) * 3;
-  }
+  calculateNewColor(wheelPos, &red, &green, &blue);
 
   for(uint8_t i = 0; i < pixelCnt; ++i)
   {
@@ -135,22 +149,7 @@ void colorMngrApplyRangeTrail(uint32_t trailStart, uint8_t wheelStart,
 
   while(pixelCntr < pixelCnt)
   {
-    if(wheelPos < COLOR_WHEEL_BLU_TO_GRN)
-    {
-      red = 255 - wheelPos * 3;
-      blue = wheelPos * 3;
-    }
-    else if(wheelPos >= COLOR_WHEEL_BLU_TO_GRN &&
-      wheelPos < COLOR_WHEEL_GRN_TO_RED)
-    {
-      blue = 255 - (wheelPos - COLOR_WHEEL_BLU_TO_GRN) * 3;
-      green = (wheelPos - COLOR_WHEEL_BLU_TO_GRN) * 3;
-    }
-    else
-    {
-      green = 255 - (wheelPos - COLOR_WHEEL_GRN_TO_RED) * 3;
-      red = (wheelPos - COLOR_WHEEL_GRN_TO_RED) * 3;
-    }
+    calculateNewColor(wheelPos, &red, &green, &blue);
 
     pixelPntr->r = red;
     pixelPntr->g = green;
