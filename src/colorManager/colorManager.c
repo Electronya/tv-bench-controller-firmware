5441a602aade4b7f81e4d4ed35c650e2e7f670c3
jbacon <julien.bacon.1@gmail.com>
2024-02-16 18:04:51 -0500

    typo

--- src/colorManager/colorManager.c
+++ src/colorManager/colorManager.c
@@ -1,7 +1,7 @@
 /**
  * Copyright (C) 2023 by Electronya
  *
- * @file      colorManager.h
+ * @file      colorManager.c
  * @author    jbacon
  * @date      2023-12-08
  * @brief     Color Manager Module
